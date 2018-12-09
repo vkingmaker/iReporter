c5da90ed0bf7b5fa07eae000fa6d447ed805a3d4
vkingmaker <vkingmaker1@gmail.com>
2018-12-09 19:23:33 +0100

    feat(getrecordbyidRouter): add a router to get a record by id #162366613 add a router to get records by a given id [Delivers #162366613]

--- server/src/report.js
+++ server/src/report.js
@@ -3,5 +3,6 @@ import RedflagsController from './redflagsController';
 
 const router = express.Router();
 
+/* Post router */
 router.get('/red-flags/:id', RedflagsController.getRecordGivenAnId);
 module.exports = router;
