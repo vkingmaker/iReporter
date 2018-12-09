6911f9e537f2bd215e07316021669f8c0305dcf4
vkingmaker <vkingmaker1@gmail.com>
2018-12-09 20:21:30 +0100

    feat(updaterecordbyidRouter): add a router to patch a record's location #162368027 add a router to update a record's location of a given id [Delivers #162368027]

--- server/src/redflagsController.js
+++ server/src/redflagsController.js
@@ -3,10 +3,19 @@ import db from './mock/db';
 const records = db();
 
 class RedflagsController {
-  static getRecords(req, res) {
-    res.send({
+  static updateLocationGivenAnId(req, res) {
+    const locationToUpdate = records.filter((value) => {
+      if (value.id === +req.params.id) {
+        return value;
+      }
+    });
+    locationToUpdate[0].location = req.body.location;
+    res.status(200).send({
       status: res.statusCode,
-      data: [...records],
+      data: [{
+        id: +req.params.id,
+        message: 'Updated red-flag record\'s location',
+      }],
     });
   }
 }
--- test/test.js
+++ test/test.js
@@ -11,15 +11,19 @@ chai.use(chaiHttp);
 // /GET 
 // /api/v1/red-flags
 
-describe('/GET all the records', () => {
-  it('it should GET all the comments', (done) => {
+describe('/PATCH a paticular location', () => {
+  it('should Uupdate the location of a particular record', (done) => {  
+    const record = {
+      id: 1,
+      location: '10.7857834N/94.756E',
+    };
+
     chai.request(server)
-      .get('/api/v1/red-flags')
-      .end((err, res) => {
+      .patch(`/api/v1/red-flags/${record.id}/location`)
+      .send(record)
+      .end((req, res) =>{ 
         res.should.have.status(200);
-        res.body.data.should.be.a('array');
-        res.body.data[0].should.include({ comment: 'Corruption in the system and the malpratices is just draining the life out of our lovely continent' });
-        res.body.data.length.should.be.eql(2);
+        res.body.data[0].should.have.property('message').eql('Updated red-flag record\'s location');
         done();
       });
   });
