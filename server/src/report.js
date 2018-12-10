3d0ab5581cb0c983d8a486f492a307d7bae5f999
vkingmaker <vkingmaker1@gmail.com>
2018-12-10 16:58:40 +0100

    bug(apiBaseUrl): update the endpoints
    update the routers endpoints to ensure that the router middleswares that will be used will be record specific

--- server/src/report.js
+++ server/src/report.js
@@ -3,12 +3,12 @@ import RedflagsController from './redflagsController';
 
 const router = express.Router();
 
-router.delete('/red-flags', RedflagsController.clearRecord);
-router.delete('/red-flags/:id', RedflagsController.deleteRecordGivenAnId);
-router.patch('/red-flags/:id/location', RedflagsController.updateLocationGivenAnId);
-router.patch('/red-flags/:id/comment', RedflagsController.updateCommentGivenAnId);
-router.get('/red-flags/:id', RedflagsController.getRecordGivenAnId);
-router.post('/red-flags', RedflagsController.createRecord);
-router.get('/red-flags', RedflagsController.getRecords);
+router.delete('/', RedflagsController.clearRecord);
+router.delete('/:id', RedflagsController.deleteRecordGivenAnId);
+router.patch('/:id/location', RedflagsController.updateLocationGivenAnId);
+router.patch('/:id/comment', RedflagsController.updateCommentGivenAnId);
+router.get('/:id', RedflagsController.getRecordGivenAnId);
+router.post('/', RedflagsController.createRecord);
+router.get('/', RedflagsController.getRecords);
 
 module.exports = router;
