f6de954670a8cc64882b4bdf6a4c5864e1a68d01
vkingmaker <vkingmaker1@gmail.com>
2018-12-11 15:52:58 +0100

    feat(signUpRouter): add an end point for signUp #162533592 add a post router to enable user's sign up to be able to use the application [Delivers #162533592]

--- server/src/report.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import RedflagsController from './redflagsController';
-
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', RedflagsController.getRecords);
-module.exports = router;
