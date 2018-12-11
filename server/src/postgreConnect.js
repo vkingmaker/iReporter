527707614b08abcb823bb0fc8afb3ae8d1c15201
vkingmaker <vkingmaker1@gmail.com>
2018-12-11 15:38:42 +0100

    feat(signUpRouter): add an end point for signUp #162533592 add a post router to enable user's sign up to be able to use the application [Delivers #162533592]

--- server/src/postgreConnect.js
+++ server/src/postgreConnect.js
@@ -4,7 +4,7 @@ const client = new Client({
   connectionString: process.env.DATABASE || 'postgres://Monday:akubudike1!@localhost/i-reporter',
 });
 client.connect()
-  .then(() => console.log('connected'))
-  .catch(err => console.error('connection error', err.stack));
+  .then(() => ('connected'))
+  .catch(err => (`connection error ${err.stack}`));
 
 module.exports = client;
