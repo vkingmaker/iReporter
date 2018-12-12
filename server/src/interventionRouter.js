5844966ef835db7e90ce153b2bac4966ae8dc8be
vkingmaker <vkingmaker1@gmail.com>
2018-12-12 13:46:02 +0100

    feat(retrieveInterventionRoute):add a route to retrieve intervention #162533726 add a GET route to enable users retrieve all the interventions [Deliver #162533726]

---
+++ server/src/interventionRouter.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import InterventionController from './interventionController';
+
+const router = express.Router();
+
+router.post('/auth/signup', InterventionController.signUp);
+router.post('/auth/login', InterventionController.login);
+router.post('/', InterventionController.createIntervention);
+router.get('/', InterventionController.retrieveAllInterventions);
+module.exports = router;
